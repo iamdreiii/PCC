ae225d18b20de882568a975f733e63fe8bc3a43c
Marc Andrei Gobison <marcandrei.gobison@gmail.com>
2023-04-14 16:22:22 +0800

    blog(fixed up img vid edit, plus smmrnte

--- application/views/admin/blog/modals/modals.php
+++ application/views/admin/blog/modals/modals.php
@@ -1,44 +1,44 @@
-<div class="modal fade" id="blog_modal_form" role="dialog" tabindex="-1">
-    <div class="modal-dialog " role="document" style="width:500px;">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h3><i class="glyphicon glyphicon-file"></i> <span class="modal-title"></span></h3>
-            </div>
-            <div class="modal-body">
-                <form action="#" id="form">
-                    <input type="hidden" value="" name="id"/>
-                    <div class="form-body">
-                            <div class="row">
-                                <label class="col-xs-3" for="school_year">Title <b style="color:red;">*</b></label>
-                                <div class="col-xs-8">
+<!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
+
+
+<div class="modal fade" id="blog_modal_form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content ">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form action="#" id="form">
+                            <input type="hidden" value="" name="id"/>
+                            <div class="form-group">
+                                <label for="school_year">Title <b style="color:red;">*</b></label>
                                     <input name="title" placeholder="Title" class="form-control" type="text">
                                     <span class="help-block"></span>
-                                </div>
+                                
                             </div>
-                            <div class="row">
-                                <label class="col-xs-3" for="path">File <b style="color:red;">*</b></label>
-                                <div class="col-xs-8">
+                            <div class="form-group">
+                                <label  for="path">File <b style="color:red;">*</b></label>
                                     <input type="file" name="path" id="path" class="form-control">
                                     <span class="help-block"></span>
-                                </div>
                             </div>
-                            <div class="row">
-                                <label class="col-xs-3" for="status">Description <b style="color:red;">*</b></label>
-                                <div class="col-xs-8">
-                                    <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
+                            <div class="form-group">
+                                    <!-- <textarea name="description" id="description" placeholder="Enter Announcement Details Here..." class="form-control" style="width: 100%; height: 500px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea> -->
+                                    <textarea  name="description" id="description" cols="30" rows="20">Enter Details Here...</textarea>
+                                    <!-- <div id="description" name="description"></div> -->
                                     <span class="help-block"></span>
-                                </div>
+                                    <script>
+                                    $('#description').summernote({height: 300});
+                                    </script>
                             </div>
-                    </div>
                 </form>
-            </div><br><br>
-            <div class="modal-footer">
+      </div>
+      <div class="modal-footer">
                 <button type="button" class="btn btn-danger pull-center" data-dismiss="modal"><i class="glyphicon glyphicon-remove"></i> Cancel</button>
                 <button type="button" id="btnSave" onclick="save()" class="btn btn-primary pull-center"><i class="glyphicon glyphicon-save"></i> Save</button>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      </div>
+    </div>
+  </div>
+</div>
